5aecf8e1edaabef3b3e7ec0173fbef3c46260fb4
mattviolet <mattviolet@gmail.com>
2020-04-27 16:27:44 -0700

    Implement responsive About section

--- src/components/About.vue
+++ src/components/About.vue
@@ -5,13 +5,12 @@
       <div class="bg-div">
         
         <div class="photo-div">
-          <div class="hover-text-div">
-            <p class="hover-text">That's an insulin pump and blood glucose sensor. Together they create a continuous glucose monitoring (CGM) system, AKA an "artificial pancreas".</p>
-          </div>
-          <img class="photo" src="../assets/showing-pump.jpg">
+          <p class="photo-text">
+            That's an insulin pump and blood glucose sensor. Together they create a continuous glucose monitoring (CGM) system, AKA an "artificial pancreas".
+          </p>
         </div>
         
-        <div class="text">
+        <div class="about-text-div">
           <p><strong>I never could have anticipated my journey into software development,</strong> but now I can't see myself doing anything else.</p>
           <p>Although my professional path has taken many twists and turns — from tutoring students of East Oakland's underserved schools, to empowering Burmese migrants in Thailand, to establishing a new communications department for an activist non-profit in Berkeley — <strong>I've always followed my passion</strong> to help others and solve problems.</p>
           <p>My passion for programming comes from the role software plays in my personal health, as a <strong>self-identified <a href='https://drive.google.com/file/d/1D8tp35PoCCbMzuWLTPHFI5oGN7nFhIQ1/view?usp=sharing' target='_blank'>human cyborg</a></strong> who has used an <strong>"artificial pancreas"</strong> to manage his type one diabetes since 2016. With my daily well-being now literally tied to algorithms, I'm invested in learning and developing applications that have the potential to help others as much as these devices have helped me.</p>
@@ -30,20 +29,6 @@ export default {
 </script>
 
 <style scoped>
-#about {
-  background: white;
-}
-.inner-content {
-  padding: 75px;
-}
-.bg-div {
-  padding: 30px;
-  background: rgb(238, 238, 238);
-  border-radius: 5px;
-  display: table;
-  table-layout: fixed;
-  /* width: 100%; */
-}
 h1 {
   padding: 30px 75px;
   font-size: 50px;
@@ -60,46 +45,54 @@ p {
 strong {
   font-weight: 900;
 }
-.photo-div {
-  width: 300px;
-  height: 369.38px;
+#about {
+  background: white;
+}
+.inner-content {
+  padding: 75px;
+}
+.bg-div {
+  padding: 30px 30px 16px 30px;
+  background: rgb(238, 238, 238);
+  border-radius: 5px;
+  margin: 0 10px 10px 0;
   overflow: hidden;
-  display:table-cell;
+}
+.photo-div {
+  width: 260px;
+  height: 319px;
+  float: left;
+  margin: 0 20px 0px 0;
+  overflow: hidden;
   border-radius: 5px;
+  background-image: url('../assets/showing-pump.jpg');
+  background-size: contain;
+  background-repeat: no-repeat;
 }
-.photo {
-  border-radius: 5px;
-  box-shadow: 1px 2px 8px 0px grey;
-  width: 300px;
-  display: block;
-}
-.text {
-  vertical-align: top;
-  width: 100%;
-  padding: 0 20px;
-  display: inline-block;
-  font-size: 16px;
-  display:table-cell;
-}
-.hover-text-div {
-  position: absolute;
-  opacity: 0;
-  padding: 30px;
-  border-radius: 5px;
-  background: rgba(48, 48, 48);
-  width: 240px;
-  height: 309.375px;
-}
-.hover-text {
-  margin-top: 45%;
-  font-size: 16px;
-  font-weight: 500;
-  color: white;
-  position: relative;
-  top: 10;
-}
-.hover-text-div:hover {
+.photo-div:hover .photo-text {
   opacity: .9;
   transition: .5s;
+}
+.photo-text {
+  opacity: 0;
+  width: 240px;
+  height: 100%;
+  margin: 0;
+  top: 10;
+  padding: 45% 10px 0px 10px;
+  text-align: center;
+  font-size: 14px;
+  font-weight: 500;
+  color: white;
+  background: rgb(0, 0, 0, .8);
+}
+.about-text-div {
+  vertical-align: top;
+  font-size: 16px;
+}
+@media (max-width: 1100px) {
+  .about-text-div {
+    font-size: 14px;
+  }
 }
 </style>
